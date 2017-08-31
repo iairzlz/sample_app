56b5f97e1fbc374a7b82fa984481f05c27b69c18
iair <iairzlz@163.com>
2017-08-31 20:51:26 +0800

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
 end
